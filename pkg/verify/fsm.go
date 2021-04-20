c236bfd2d5f2e5e8cc2cfc7a2439d2c9d6868f78
Eric Moynihan <git@moynihan.io>
2021-04-20 19:38:32 +0000

    Added FSM params

--- pkg/verify/fsm.go
+++ pkg/verify/fsm.go
@@ -1,20 +1,46 @@
 package verify
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"fmt"
 
-var state = map[string]func() error{}
+	"github.com/bwmarrin/discordgo"
+)
+
+type stateMap map[string]func(m *StateParams) error
+
+func (s stateMap) get(m *StateParams) (func(m *StateParams) error, bool) {
+	if m == nil {
+		return nil, false
+	}
+	out, ok := s[m.User.ID+m.Guild.ID]
+	return out, ok
+}
+
+var (
+	state   = stateMap{}
+	session *discordgo.Session
+)
+
+func Init(s *discordgo.Session) {
+	session = s
+}
+
+type StateParams struct {
+	User  *discordgo.User
+	Guild *discordgo.Guild
+}
 
 // Transition from one state to another base on the decision tree.
-func Transition(m *discordgo.Message) error {
-	switch m.Content {
-	default:
-		if err := createLink(); err != nil {
-			return err
-		}
+func Transition(s *StateParams) error {
+	if handler, ok := state.get(s); ok {
+		handler(s)
+	} else if err := createLink(s); err != nil {
+		return err
 	}
 	return nil
 }
 
-func createLink() error {
+func createLink(m *StateParams) error {
+	session.ChannelMessageSend(m.User.ID, fmt.Sprint("To register for %s, click on the following link: %s"))
 	return nil
 }
