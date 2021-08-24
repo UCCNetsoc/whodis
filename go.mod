28978ca7493c05511b23ca33a4b3df11ecac7667
Eric Moynihan <git@moynihan.io>
2021-08-24 15:24:30 +0000

    commit rest of WIP configs

--- go.mod
+++ go.mod
@@ -6,11 +6,12 @@ require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
 	github.com/boj/redistore v0.0.0-20180917114910-cd5dcc76aeff // indirect
-	github.com/bwmarrin/discordgo v0.23.3-0.20210312144535-ba10a00fbcfa
+	github.com/bwmarrin/discordgo v0.23.3-0.20210529215543-f5bb723db8d9
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19
 	github.com/gin-gonic/gin v1.6.3
 	github.com/gorilla/sessions v1.2.1 // indirect
+	github.com/hashicorp/hcl v1.0.0
 	github.com/lib/pq v1.3.0
 	github.com/spf13/viper v1.7.1
 	github.com/swaggo/files v0.0.0-20190704085106-630677cd5c14
--- pkg/commands/config.go
+++ pkg/commands/config.go
@@ -11,9 +11,14 @@ import (
 
 var configOptions = []*discordgo.ApplicationCommandOption{
 	{
+		Name:        "roles",
+		Description: "List of roles to be added when user is authenticated.",
+		Type:        discordgo.ApplicationCommandOptionSubCommand,
+	},
+	{
 		Name:        "domains",
 		Description: "List of email domains google accounts being verified can have. Leave blank for all domains. Can be separeted by a space and/or comma.",
-		Type:        discordgo.ApplicationCommandOptionString,
+		Type:        discordgo.ApplicationCommandOptionSubCommand,
 	},
 }
 
@@ -38,6 +43,11 @@ func ConfigCommand(s *discordgo.Session, i *discordgo.InteractionCreate) (string
 				if value, userErr = parseDomains(value.(string)); userErr != "" {
 					return userErr, nil
 				}
+			case "roles":
+				var userErr string
+				if value, userErr = parseList(value.(string)); userErr != "" {
+					return userErr, nil
+				}
 			}
 			if err := models.DBClient.SetConfigItem(i.GuildID, name, value); err != nil {
 				return "Internal error occured.", err
@@ -49,14 +59,25 @@ func ConfigCommand(s *discordgo.Session, i *discordgo.InteractionCreate) (string
 }
 
 func parseDomains(value string) ([]string, string) {
+	return parseList(value, func(value string) string {
+		if !govalidator.IsDNSName(value) {
+			return fmt.Sprintf("%s is not a valid DNS name", value)
+		}
+		return ""
+	})
+}
+
+func parseList(value string, filter ...func(value string) string) ([]string, string) {
 	if strings.Contains(value, ",") {
 		value = strings.ReplaceAll(value, ",", " ")
 	}
-	domains := strings.Fields(value)
-	for _, domain := range domains {
-		if !govalidator.IsDNSName(domain) {
-			return nil, fmt.Sprintf("%s is not a valid DNS name", domain)
+	fields := strings.Fields(value)
+	for _, field := range fields {
+		if len(filter) > 0 {
+			if strErr := filter[0](field); strErr != "" {
+				return nil, strErr
+			}
 		}
 	}
-	return domains, ""
+	return fields, ""
 }
