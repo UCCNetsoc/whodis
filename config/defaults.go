607b8859ebdfe1bbb4e785f7962ed3bb8c914b69
ReeceDonovan <reecedonovan99@gmail.com>
2021-08-27 15:26:43 +0100

    Removed postgres db
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- config/defaults.go
+++ config/defaults.go
@@ -3,18 +3,15 @@ package config
 import "github.com/spf13/viper"
 
 func initDefaults() {
-	viper.SetDefault("api.title", "Veribot API")
+	viper.SetDefault("api.title", "Whodis API")
 	viper.SetDefault("api.description", "API to authenticate/authorize discord users")
 	viper.SetDefault("api.version", "0.1")
 	viper.SetDefault("api.path", "/api/v1")
 	viper.SetDefault("api.hostname", "whodis.netsoc.cloud")
-	viper.SetDefault("api.sluglength", 5)
+	viper.SetDefault("api.secret", "")
+
+	viper.SetDefault("oauth.google.id", "")
+	viper.SetDefault("oauth.google.secret", "")
 
 	viper.SetDefault("discord.token", "")
-
-	viper.SetDefault("db.host", "postgres")
-	viper.SetDefault("db.port", "5432")
-	viper.SetDefault("db.user", "root")
-	viper.SetDefault("db.pass", "password")
-	viper.SetDefault("db.name", "promexporter")
 }
