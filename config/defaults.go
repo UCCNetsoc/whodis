a4d081337fa2aac021164fa4a09b0345664e0b94
ReeceDonovan <reecedonovan99@gmail.com>
2021-08-30 23:01:16 +0100

    Cleaning up unused config variables
    Signed-off-by: ReeceDonovan <reecedonovan99@gmail.com>

--- config/defaults.go
+++ config/defaults.go
@@ -3,11 +3,7 @@ package config
 import "github.com/spf13/viper"
 
 func initDefaults() {
-	viper.SetDefault("api.title", "Whodis API")
-	viper.SetDefault("api.description", "API to authenticate/authorize discord users")
-	viper.SetDefault("api.version", "0.1")
-	viper.SetDefault("api.path", "/api/v1")
-	viper.SetDefault("api.hostname", "whodis.netsoc.cloud")
+	viper.SetDefault("api.hostname", "")
 	viper.SetDefault("api.secret", "")
 
 	viper.SetDefault("oauth.google.id", "")
